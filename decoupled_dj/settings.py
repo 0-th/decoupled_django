d83066719743e9034abe3a65735b6e2abd858f19
0-th <artemisfowlx@protonmail.com>
2023-07-26 03:04:56 +0100

    delete exposed secret key

--- decoupled_dj/settings.py
+++ decoupled_dj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nxtymo$a%e_n583_mh3u16rz#ja3n$qwy4e&el_g@y_@i&0p0+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
